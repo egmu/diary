cb91b4a98951f091b62d34f268e64e502b22ab98
egmu <mkh.yusuf@gmail.com>
2022-12-21 06:21:54 +0700

    first

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: node . --db 'mongodb+srv://egmu:egmu12345678@cluster0.7viqfsb.mongodb.net/?retryWrites=true&w=majority'
+
---
+++ plugins/mode-bot.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+  const ultah = new Date('Juli 11 2023 00:0:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+ await conn.sendButton(m.chat, `*Hai Ngab*
+Mau makan Mie goreng\n`,wm + '\n\n' + botdate, thumbdoc, [['Aʟʟ Mᴇɴᴜ','.? all'],['Lɪsᴛ Mᴇɴᴜ','.siuuu']], m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `A MIMIR🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Join Grup`,          previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/5st560k/fake.jpg`)).buffer(),
+                        sourceUrl: 'https://chat.whatsapp.com/Ib4JOndTWQmGBSsPGWmIup'
+                      }}
+})
+}
+
+
+handler.help = ['menu']
+handler.tags = ['group']
+handler.command = /^(menu|help|co)$/i
+handler.register = false
+
+export default handler
